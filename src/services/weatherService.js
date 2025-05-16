34d7f1a56ccbb803bbf19984e611b523ffb430f9
Brian <brian@briansmusic.com>
2025-05-16 17:15:38 -0600

    ensure api call should function correctly

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
-const API_KEY = process.env.REACT_APP_OPENWEATHERMAP_API_KEY
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
+
+const API_KEY = process.env.VUE_APP_API_KEY
+const BASE_URL = process.env.VUE_APP_API_BASE_URL
+
 export const fetchWeather = async (lat, lon) => {
   const url = `${BASE_URL}?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
   const response = await axios.get(url)
