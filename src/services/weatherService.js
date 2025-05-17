8910b020398924d7f2e26ec0cc6e382b1439bfe3
Brian <brian@briansmusic.com>
2025-05-16 18:03:28 -0600

    unhide api key add cities csv

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const API_KEY = process.env.VUE_APP_API_KEY
-const BASE_URL = process.env.VUE_APP_API_BASE_URL
+const API_KEY = '95785a793dd8ff2fbcdcd608576d39b8'
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/onecall'
 
 export const fetchWeather = async (lat, lon) => {
   const url = `${BASE_URL}?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
