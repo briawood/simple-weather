5b9e368fdad14701fca22c2da781a82640e71a5b
Brian <brian@briansmusic.com>
2025-05-16 20:54:40 -0600

    get api working

--- src/components/WeatherDisplay.vue
+++ src/components/WeatherDisplay.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
-    <h2>Current: {{ weatherData.current.temp }}°C</h2>
-    <img :src="iconUrl(weatherData.current.weather[0].icon)" />
+    <h2>Current: {{ weatherData.list[0].main.temp }}°C</h2>
+    <img :src="iconUrl(weatherData.list[0].weather[0].icon)" />
 
     <h3>Next 6 Hours</h3>
     <div class="forecast-row">
-      <div v-for="(hour, i) in weatherData.hourly.slice(0, 6)" :key="i">
+      <div v-for="(hour, i) in weatherData.list.slice(0, 6)" :key="i">
         <p>{{ formatHour(hour.dt) }}</p>
         <img :src="iconUrl(hour.weather[0].icon)" />
-        <p>{{ hour.temp }}°C</p>
+        <p>{{ hour.main.temp }}°C</p>
       </div>
     </div>
 
     <h3>Next 5 Days</h3>
     <div class="forecast-row">
-      <div v-for="(day, i) in weatherData.daily.slice(0, 5)" :key="i">
+      <div v-for="(day, i) in weatherData.list.slice(0, 5)" :key="i">
         <p>{{ formatDay(day.dt) }}</p>
         <img :src="iconUrl(day.weather[0].icon)" />
-        <p>{{ day.temp.day }}°C</p>
+        <p>{{ day.main.temp.day }}°C</p>
       </div>
     </div>
   </div>
--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API_KEY = '95785a793dd8ff2fbcdcd608576d39b8'
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/onecall'
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/forecast'
 
 export const fetchWeather = async (lat, lon) => {
   const url = `${BASE_URL}?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`
